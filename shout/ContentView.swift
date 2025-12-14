ef710b985876e31b35960d940c5e9b39d0a8fb37
eribas <ribasedu2005@gmail.com>
2025-12-14 14:33:09 -0300

    Initial Commit

---
+++ shout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shout
+//
+//  Created by Eduardo Ribas on 14/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shout/shoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoutApp.swift
+//  shout
+//
+//  Created by Eduardo Ribas on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct shoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
